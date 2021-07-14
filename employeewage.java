1dab83b7c65a3ab5f298946ae229f1ebb4b1f992
kaviyasree99 <kaviyarajalingam@gmail.com>
2021-07-14 23:36:10 +0530

    Deleting unwanted file and ombining code

--- employeewage.java
+++ employeewage.java
@@ -1,8 +1,21 @@
 public class employeewage
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Welcome to employee wage computation!" );
+        int IS_FULL_TIME = 1 ;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 2 ;
+
+              if (empCheck == IS_FULL_TIME)
+                 empHrs = 8;
+
+              else 
+                 empHrs = 0;
+
+          empWage = empHrs * EMP_RATE_PER_HOUR;
+          System.out.println("Emp wage: "+ empWage);
     }
 }
 
